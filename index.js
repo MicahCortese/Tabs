da4224aa2ee23d85fd67c98419dcc22bc759a2df
corcheesey <micahcortese@gmail.com>
2025-01-29 20:43:15 -0500

    Created a simple tabs component using HTML, CSS, and JavaScript in order to learn basic DOM manipulation and event handling.

---
+++ index.js
@@ -0,0 +1,17 @@
+function displayTab1() {
+    document.getElementById("textarea").innerHTML = "This is the first tab!";
+}
+
+function displayTab2() {
+    document.getElementById("textarea").innerHTML = "This is the second tab!";
+}
+
+function displayTab3() {
+    document.getElementById("textarea").innerHTML = "This is the third tab!";
+}
+
+function displayTab4() {
+    document.getElementById("textarea").innerHTML = "This is the fourth tab!";
+}
+
+window.onload = displayTab1;
